94de424ee8c76bab0b72e4aebdad5c7dd7fba278
Thais Minet <thais@minet.dk>
2016-09-18 11:25:01 -0500

    dlkdkj

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
     <h1>hello</h1>
     <?php
     echo "My first PHP script!";
+    #Database=cudb;Data Source=us-cdbr-azure-central-a.cloudapp.net;User Id=b3749d9a9bbf00;Password=c55f1efd
+    $servername = "cudb.us-cdbr-azure-central-a.cloudapp.net";
+    $username = "b3749d9a9bbf00";
+    $password = "c55f1efd";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "Connected successfully";
     ?>
 </body>
 </html>
