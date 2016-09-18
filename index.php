8707322f7a5864f0bbc7b7adedb5af5533acb738
Thais Minet <thais@minet.dk>
2016-09-18 11:58:32 -0500

    sakjd

--- index.php
+++ index.php
@@ -18,11 +18,11 @@
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
 
-    // Check connection
-    // if ($conn->connect_error) {
-    //     die("Connection failed: " . $conn->connect_error);
-    // } 
-    // echo "Connected successfully";
+    Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "Connected successfully";
 
     // $sql = "INSERT INTO Persons (PersonID, LastName, FirstName, Address, City)
     // VALUES (1, 'Smith', 'Erik','1 University Dr','Nashville')";
