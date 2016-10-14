bd5cbe4f64942a5afb41370c0b566f92b53c92bf
Connor Austin <arcademissile@gmail.com>
2016-10-14 14:11:51 -0500

    Added header file
    What what!

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
     <title>credit union test page</title>
 </head>
 <body>
-
+	<?php include_once "header.php"; ?>
     <h1>hello</h1>
-    <p>This is a test paragraph. Penis - it's gonna be huge</p>
-    
+    <p>This is a test paragraph. Penis - it's gonna be huge</p>    
 </body>
 </html>
