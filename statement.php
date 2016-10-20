215d98ee387208e4f22a2b69ba9d7eb0b865065e
Connor Austin <arcademissile@gmail.com>
2016-10-19 22:36:03 -0500

    Begun working on body

--- statement.php
+++ statement.php
@@ -1,16 +1,17 @@
-<style>
-.statement-header {
-	font-family: federation;
-	text-align: center;
-	font-size: 100px;
-}
-.statement-sub-header {
-	font-family: federation;
-	text-align: center;
-	font-size: 25px;
-	position: relative;
-	top: -30px;
-}
-</style>
-<div class="statement-header">ICU</div>
-<div class="statement-sub-header">build a better way to do credit</div>
+<link rel="stylesheet" href="style.php/statement.scss">
+
+<div class="statement-title">ICU</div>
+<div class="statement-sub-title"><i>build a better way to do credit</i></div>
+
+<div class="container">
+	<div class="row statement-header">
+		<div class="col-md-4">UNIVERSAL</div>
+		<div class="col-md-4">INNOVATIVE</div>
+		<div class="col-md-4">REQUIRED</div>
+	</div>
+	<div class="row statement">
+		<div class="col-md-4">We provide services to the entire galaxy....</div>
+		<div class="col-md-4">We are the only galatic credit union to....</div>
+		<div class="col-md-4">The imperial whatever enforces the use of this for security...</div>
+	</div>
+</div>
