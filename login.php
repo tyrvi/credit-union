e146e788d762d20d3e92b90a08e31053d0ab1962
Thais Minet <thais@minet.dk>
2016-10-24 20:25:49 -0500

    created template pages for login, invest, and apply

--- login.php
+++ login.php
@@ -1,10 +1,13 @@
 <!DOCTYPE <html>
 <html>
 <head>
-    <meta charset="UTF-8">
+    <?php include_once "includes.php" ?>
     <title>Log in</title>
 </head>
 <body>
-    
+	<?php include_once "navbar.php"; ?>
+	<div id="main">
+		<!-- Content -->
+	</div>
 </body>
 </html>
