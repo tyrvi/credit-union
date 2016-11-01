5009df3c3c67fee607f9b196a8909118ed89915f
Thais Minet <thais.minet@moodwire.com>
2016-11-01 14:42:46 -0500

    made some adjustments to the form

--- apply.php
+++ apply.php
@@ -10,18 +10,18 @@
 		<!-- Content -->
 		<div>
 			<form>
-				<input type="text" name="Fname" value="First Name"></br>
-				<input type="text" name="Mname" value="Middle Name"></br>
+				<input type="text" name="Fname" value="First Name">
+				<input type="text" name="Mname" value="Middle Name">
 				<input type="text" name="Lname" value="Last Name"></br>
-				<input type="text" name="Address1" value="Address 1"></br>
+				<input type="text" name="Address1" value="Address 1">
 				<input type="text" name="Address2" value="Address 2"></br>
-				<input type="text" name="City" value="City"></br>
+				<input type="text" name="City" value="City">
 				<select>
 					<option value="TA">TA - Tatooine</option>
 					<option value="DA">DA - Dantooine</option>
 					<option value="KA">KA - Kashyyyk</option>
 				</select></br>
-				<input type="date" name="DOB" value="Date of Birth"></br>
+				Date of Birth: <input type="date" name="DOB" value="Date of Birth">
 				<input type="text" name="SS" value="Social Security"></br>
 				<input type="password" name="Pass" value="Password"></br>
 			</form>
