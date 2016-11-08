e9b43949a8e0135a05babdc18e94ab2f66a7012c
Thais Minet <thais@minet.dk>
2016-11-07 19:55:36 -0600

    Finally!!! You can see an SQL query!!!

--- apply.php
+++ apply.php
@@ -35,19 +35,39 @@
 			</form>
 		</div>
 		<?php
-			$link = mysqli_connect("us-cdbr-azure-central-a.cloudapp.net", "b3749d9a9bbf00", "c55f1efd", "cudb");
+			$mysqli = new mysqli("us-cdbr-azure-central-a.cloudapp.net", "b3749d9a9bbf00", "c55f1efd", "cudb");
+			if ($mysqli->connect_errno) {
+				echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+			}
+			echo $mysqli->host_info . "\n";
 
+			$res = $mysqli->query("select * from cudb.customers where C_Id=1;");
+
+			$row = $res->fetch_assoc();
+			echo gettype($row);
+			
+			foreach($row as $item) {
+				echo "<br>";
+				echo $item;
+			}
+			
+			
+			//printf("C_id = %s (%s)\n", $row['C_id'], gettype($row['C_Id']));
+			/*
+			$link = mysqli_connect("us-cdbr-azure-central-a.cloudapp.net", "b3749d9a9bbf00", "c55f1efd", "cudb");
+			
 			if (!$link) {
 				echo "Error: Unable to connect to MySQL.".PHP_EOL;
 				echo "Debugging errno: ".mysqli_connect_errno().PHP_EOL;
 				echo "Debugging error: ".mysqli_connect_error().PHP_EOL;
 				exit;
 			}
-
+			
 			echo "Success: A proper connection to MySQL was made! The my_db database is great.".PHP_EOL;
 			echo "Host information: ".mysqli_get_host_info($link).PHP_EOL;
 
 			mysqli_close($link);
+			*/	
 		?>
 	</div>
 </body>
