b5eb2d602c5b8b0cfa0e0982f883a2363292c3d4
Wiemanizer <brycewieman@gmail.com>
2016-11-07 19:36:46 -0600

    Created about page

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE <html>
+<html>
+<head>
+    <?php include_once "includes.php" ?>
+    <title>About</title>
+</head>
+<body>
+	<?php include_once "navbar.php"; ?>
+	<div id="main">
+	Heerearea
+	</div>
+</body>
+</html>
