f1bc9e426c9e6b64d5f308a65da43c51385576a1
Connor Austin <arcademissile@gmail.com>
2016-11-15 20:24:25 -0600

    Fixed query issue

--- includes.php
+++ includes.php
@@ -1,5 +1,5 @@
 <!-- jQuery -->
-<script   src="http://code.jquery.com/jquery-3.1.1.min.js"   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="   crossorigin="anonymous"></script>
+<script   src="http://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
 
 <!-- Bootstrap includes -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
