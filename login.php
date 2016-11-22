6d5c588edf52c58d1c69b81b4671154a80f7a8e1
Connor Austin <arcademissile@gmail.com>
2016-11-22 11:21:34 -0600

    Fixed login bug, redirect works

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+	// open connection to database
+	$mysqli = new mysqli("us-cdbr-azure-central-a.cloudapp.net", "b3749d9a9bbf00", "c55f1efd", "cudb");
+	
+	// check for database connection error
+	if ($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+
+	$username = $password = "";
+	
+	// TO-DO: these nested statements are so bad need to fix them at some point
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$username = $_POST["username"];
+		$password = $_POST["pass"];
+
+		$res = $mysqli->query("SELECT Email, Pass from cudb.customers where Email = '$username'");
+		$row = $res->fetch_assoc();
+		
+		if ($row === NULL || $row["Pass"] != $password) {
+			$GLOBALS['badLogin'] = true;
+		}
+		else {
+			header('Location: account.php');
+			$GLOBALS['badLogin'] = false;
+		}
+	}
+	mysqli_close($mysqli);
+?>
 <!DOCTYPE <html>
 <html>
 <head>
@@ -27,38 +56,18 @@
 						<div class="form-group">
 							<input class="form-control" type="submit" name="submit" value="Login">
 						</div>
+						<div>
+						<?php 
+							if($GLOBALS['badLogin']) {
+								echo 'Incorrect password/email';
+							}
+						?>
+						</div>
 					</form>
 				</div>
 			</div>
 		</div>
-		<?php
-			// open connection to database
-			$mysqli = new mysqli("us-cdbr-azure-central-a.cloudapp.net", "b3749d9a9bbf00", "c55f1efd", "cudb");
-			// check for database connection error
-			if ($mysqli->connect_errno) {
-				echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-			}
-
-			$username = $password = "";
-
-			// TO-DO: these nested statements are so bad need to fix them at some point
-			if ($_SERVER["REQUEST_METHOD"] == "POST") {
-				$username = $_POST["username"];
-				$password = $_POST["pass"];
-
-				$res = $mysqli->query("SELECT Email, Pass from cudb.customers where Email = '$username'");
-				$row = $res->fetch_assoc();
-
-				if ($row["Pass"] != $password) {
-					echo "Incorrect email or password please try again.";
-				}
-				else {
-					echo "Congratulations you remembered your username and password!";
-				}
-				
-			}
-			mysqli_close($mysqli);
-		?>
+		
 		<?php include_once "footer.php"; ?>
 	</div>
 </body>
