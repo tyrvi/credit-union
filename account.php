b951a045ef85a5224793a5fcc5f58f9876df05fe
Thais Minet <thais@minet.dk>
2016-11-22 15:09:00 -0600

    added form for updating info

--- account.php
+++ account.php
@@ -45,10 +45,23 @@
 			</div>
 			<!-- user info -->
 			<div>
-				
+				<form>
+					<input type="text" name="Fname" value="<?php echo $_SESSION['Fname']; ?>">
+					<input type="text" name="Mname" value="<?php echo $_SESSION['Mname']; ?>">
+					<input type="text" name="Lname" value="<?php echo $_SESSION['Lname']; ?>">
+					<input type="text" name="email" value="<?php echo $_SESSION['Email']; ?>">
+					<input type="password" name="pass" value="<?php echo $_SESSION['Pass']; ?>">
+					<input type="text" name="Address1" value="<?php echo $_SESSION['Address1']; ?>">
+					<input type="text" name="Address2" value="<?php echo $_SESSION['Address2']; ?>">
+					<input type="text" name="City" value="<?php echo $_SESSION['City']; ?>">
+					<select name="Planet">
+						<option name="<?php echo $_SESSION['Planet']; ?>" placeholder="<?php echo $_SESSION['Planet']; ?>"><?php echo $_SESSION['Planet']; ?></option>
+					</select>
+					<input type="submit" name="update" value="update">
+				</form>
 			</div>
 		</div>
-		<!-- star wars add -->
+		<!-- star wars ad -->
 		<div>
 			
 		</div>
