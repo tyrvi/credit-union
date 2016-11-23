107b844cecf7d7f202c5a6f3f50c0baad022f909
Thais Minet <thais@minet.dk>
2016-11-22 22:30:39 -0600

    added testimonials

--- recommendations.php
+++ recommendations.php
@@ -1,18 +1,34 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-6">
-		Hello	
+			<p>
+				Darth Vader – “When I was looking to take out a loan, I knew that as a member of the empire, 
+				that the Imperial Credit Union was the best option! They offer the lowest interest rates in 
+				the galaxy, and made my experience a real pleasure. Thanks, ICU!”
+			</p>
 		</div>
 		<div class="col-md-6">
-		Hello	
+			<p>
+				Emperor Palpatine – “Being Dark Lord of the Sith is great, but I know it’s only a matter of 
+				time before my apprentice challenges me to a duel in a move to take the mantle in accordance 
+				with the Rule of Two. Because of this, the folks at ICU have been great in helping me plan 
+				for retirement. I know that when the time comes, I’ll be ready.”
+			</p>	
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-6">
-		Hello	
+			<p>
+				Stormtrooper No. 5C87 – “As stormtrooper, I’m used to failure. Whether it’s missing shots at point 
+				blank range, being Jedi Mind tricked, or banging my head on doors (see photo above). But the 
+				ICU doesn’t care about that. They’ve treated me and my other million clone counterparts the 
+				same- with respect, and I know they’ll do the same for you.”
+			</p>
 		</div>
 		<div class="col-md-6">
-		Hello	
+			<p>
+				Admiral Ackbar – “IT’S A TRAP!”
+			</p>	
 		</div>
 	</div>
 </div>
