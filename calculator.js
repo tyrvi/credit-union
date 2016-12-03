69a544dc666254ef4dc015866613db7b80b33f64
Thais Minet <thais@minet.dk>
2016-12-03 14:56:48 -0600

    monthly amount working

--- calculator.js
+++ calculator.js
@@ -1,8 +1,25 @@
 var app = angular.module("app", []);
 
 app.controller("ctrl", ($scope) => {
-    $scope.test = "hello";
-    $scope.testchange = () => {
-        $scope.test += "#";
+    $scope.principal = 0;
+    $scope.annualInterestRate = 1;
+    $scope.monthlyInterestRate = $scope.annualInterestRate * (12/100);
+    $scope.lengthYears = 1;
+    $scope.lengthMonths = $scope.lengthYears * 12;
+    $scope.result = 0;
+    /*
+    let P = $scope.principal;
+    let J = $scope.monthlyInterestRate;
+    let N = $scope.lengthMonths;
+    $scope.result = P * (J/(1 - (1 + J) ** -N));
+    */
+    $scope.calculate = () => {
+        let P = $scope.principal;
+        let J = $scope.monthlyInterestRate;
+        let N = $scope.lengthMonths;
+        let M = P * (J/(1 - (1 + J) ** -N));
+
+        $scope.result = M.toFixed(2);
+        //return $scope.result;
     };
 });
--- calculator.php
+++ calculator.php
@@ -9,9 +9,36 @@
 	<!--<?php include_once "navbar.php"; ?>-->
 	<div id="main">
 		<!-- Content -->
-        <input ng-change="testchange()" type="text" ng-model="test">
-          
-        <h1>{{test}}</h1>
+        <div>
+            <p>principal</p>
+            <input ng-change="calculate()" type="number" min="0" ng-model="principal">
+            <br/>
+            <br/>
+
+            <p>annual interest rate</p>
+            <input ng-change="calculate()" type="number" min="1" max="100" ng-model="annualInterestRate">
+            <br/>
+            <br/>
+
+            <p>monthly interest rate</p>
+            <input type="number" ng-model="monthlyInterestRate">
+            <br/>
+            <br/>
+
+            <p>length in years</p>
+            <input type="number" min="0" ng-model="lengthYears">
+            <br/>
+            <br/>
+
+            <p>length in months</p>
+            <input ng-change="calculate()" type="number" min="0" ng-model="lengthMonths">
+            <br/>
+            <br/>
+
+            <p></p>
+            <h1 ng-model="result">{{result}}</h1>
+        </div>
+        
         
 	</div>
     <script src="calculator.js"></script>
