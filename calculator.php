b0fce901b1951edb2adf7f071f16989b7df2d070
Connor Austin <arcademissile@gmail.com>
2016-12-04 14:45:55 -0600

    Started making calculator look less like crap

--- calculator.php
+++ calculator.php
@@ -4,50 +4,46 @@
 <head>
     <?php include_once "includes.php" ?>
     <title>Calculator</title>
+	<link rel="stylesheet" href="style.php/calculator.scss">
 </head>
 <body ng-app="app", ng-controller="ctrl">
 	<!--<?php include_once "navbar.php"; ?>-->
 	<div id="main">
-		<!-- Content -->
-        <div>
-            <p>principal</p>
-            <input ng-change="calculate()" type="number" min="0" ng-model="principal">
-            <br/>
-            <br/>
+		<div id="calculator">
+	        <div class="row">
+	            <div class="col-md-4">
+					<div class="row">
+						<p>Principal</p>
+					</div>
+					<div class="row">
+	            		<input ng-change="calculate()" type="number" min="0" ng-model="principal" class="form-control">
+					</div>
+				</div>
+				
+				<div class="col-md-4">
+	            	<p>Annual Interest Rate</p>
+	            	<input ng-change="calculate()" type="number" min="1" max="100" ng-model="annualInterestRate" class="form-control">
+				</div>
+				
+				<div class="col-md-4">
+	            <p>Length in Years</p>
+	            <input ng-change="calculate()" type="number" min="1" ng-model="lengthYears" class="form-control">
+			</div>	
+            	<p>monthly interest rate</p>
+            	<p ng-change="calculate()" ng-model="monthlyInterestRate">{{monthlyInterestRate}}</p>
+				
+	            <p>length in months</p>
+	            <p ng-change="calculate()" ng-model="lengthMonths">{{lengthMonths}}</p>
 
-            <p>annual interest rate</p>
-            <input ng-change="calculate()" type="number" min="1" max="100" ng-model="annualInterestRate">
-            <br/>
-            <br/>
-
-            <p>monthly interest rate</p>
-            <p ng-change="calculate()" ng-model="monthlyInterestRate">{{monthlyInterestRate}}</p>
-            <br/>
-            <br/>
-
-            <p>length in years</p>
-            <input ng-change="calculate()" type="number" min="1" ng-model="lengthYears">
-            <br/>
-            <br/>
-
-            <p>length in months</p>
-            <p ng-change="calculate()" ng-model="lengthMonths">{{lengthMonths}}</p>
-            <br/>
-            <br/>
-
-            <p>Monthly Payment</p>
-            <h1 ng-model="monthlyPayment">{{monthlyPayment}}</h1>
-
-            <!-- <button ng-click="calculate()">Calculate Table</button> -->
-        </div>
-
-        <div>
-            <table>
-                <tr ng-repeat="x in table track by $index">
-                    <td>{{x}}</td>
-                </tr>    
-            </table>
-        </div>
+	            <!-- <button ng-click="calculate()">Calculate Table</button> -->
+	        <div>
+	            <table>
+	                <tr ng-repeat="x in table track by $index">
+	                    <td>{{x}}</td>
+	                </tr>    
+	            </table>
+	        </div>
+		</div>
         
         
 	</div>
