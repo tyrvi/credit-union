711fac19ed033178fafc2ebece5407f6ee0dfff6
Thais Minet <thais@minet.dk>
2016-12-04 13:15:07 -0600

    while are bad

--- calculator.js
+++ calculator.js
@@ -6,21 +6,53 @@ app.controller("ctrl", ($scope) => {
     $scope.monthlyInterestRate = $scope.annualInterestRate * (12/100);
     $scope.lengthYears = 1;
     $scope.lengthMonths = $scope.lengthYears * 12;
+    $scope.table = [];
     
-    
-    $scope.yearChangeCalculate = () => {
+    function calculateTable(P, J, M) {
+        let results = [];
+        let H = 0;
+        let C = 0;
+        let Q = 0;
+
+        while (true) {
+            H = P * J;
+            C = M - H;
+            Q = P - C;
+            P = Q;
+            if (P < 0) break;
+            results.push(P);
+        };
+
+        return results;
+    }
+
+    $scope.calculate = () => {
         let X = $scope.annualInterestRate * (12/100);
         $scope.monthlyInterestRate = X.toFixed(2);
         $scope.lengthMonths = $scope.lengthYears * 12;
 
-        $scope.result = 0
         let P = $scope.principal;
         let J = $scope.monthlyInterestRate;
         let N = $scope.lengthMonths;
         let M = P * (J/(1 - (1 + J) ** -N));
 
         $scope.monthlyPayment = M.toFixed(2);
-    };
+        /*
+        let H = 0;
+        let C = 0;
+        let Q = 0;
+        var results = [];
 
+        while (true) {
+            H = P * J;
+            C = M - H;
+            Q = P - C;
+            P = Q;
+            if (P < 0) break;
+            results.push(P);
+        };
+        */
+        $scope.table = calculateTable(P, J, M);
+    };
     
 });
--- calculator.php
+++ calculator.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <?php include_once "includes.php" ?>
-    <title>TITLE</title>
+    <title>Calculator</title>
 </head>
 <body ng-app="app", ng-controller="ctrl">
 	<!--<?php include_once "navbar.php"; ?>-->
@@ -11,32 +11,42 @@
 		<!-- Content -->
         <div>
             <p>principal</p>
-            <input ng-change="yearChangeCalculate()" type="number" min="0" ng-model="principal">
+            <input ng-change="calculate()" type="number" min="0" ng-model="principal">
             <br/>
             <br/>
 
             <p>annual interest rate</p>
-            <input ng-change="yearChangeCalculate()" type="number" min="1" max="100" ng-model="annualInterestRate">
+            <input ng-change="calculate()" type="number" min="1" max="100" ng-model="annualInterestRate">
             <br/>
             <br/>
 
             <p>monthly interest rate</p>
-            <p ng-change="yearChangeCalculate()" ng-model="monthlyInterestRate">{{monthlyInterestRate}}</p>
+            <p ng-change="calculate()" ng-model="monthlyInterestRate">{{monthlyInterestRate}}</p>
             <br/>
             <br/>
 
             <p>length in years</p>
-            <input ng-change="yearChangeCalculate()" type="number" min="0" ng-model="lengthYears">
+            <input ng-change="calculate())" type="number" min="0" ng-model="lengthYears">
             <br/>
             <br/>
 
             <p>length in months</p>
-            <p ng-change="yearChangeCalculate()" ng-model="lengthMonths">{{lengthMonths}}</p>
+            <p ng-change="calculate()" ng-model="lengthMonths">{{lengthMonths}}</p>
             <br/>
             <br/>
 
             <p>Monthly Payment</p>
             <h1 ng-model="monthlyPayment">{{monthlyPayment}}</h1>
+
+            <!--<button ng-click="calculateTable()">Calculate Table</button>-->
+        </div>
+
+        <div>
+            <table>
+                <tr ng-repeat="x in table track by $index">
+                    <td>{{x}}</td>
+                </tr>    
+            </table>
         </div>
         
         
